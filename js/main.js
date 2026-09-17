e7fee9a5472634db7e1d34f6241f5ce3785dcf51
Yasir khan <imyasir.308@gmail.com>
2026-09-17 17:11:31 +0300

    Update portfolio website

--- js/main.js
+++ js/main.js
@@ -96,29 +96,31 @@
 	
 
 	var carousel = function() {
-		$('.home-slider').owlCarousel({
-	    loop:true,
-	    autoplay: true,
-	    margin:0,
-	    animateOut: 'fadeOut',
-	    animateIn: 'fadeIn',
-	    nav:false,
-	    autoplayHoverPause: false,
-	    items: 1,
-	    navText : ["<span class='ion-md-arrow-back'></span>","<span class='ion-chevron-right'></span>"],
-	    responsive:{
-	      0:{
-	        items:1
-	      },
-	      600:{
-	        items:1
-	      },
-	      1000:{
-	        items:1
-	      }
-	    }
-		});
-	};
+    $('.home-slider').owlCarousel({
+        loop: true,
+        autoplay: true,
+        margin: 0,
+        animateOut: 'fadeOut',
+        animateIn: 'fadeIn',
+        nav: false,
+        autoplayHoverPause: false,
+        items: 1,
+        touchDrag: false,   /* Prevents OwlCarousel from hijacking touch scrolling */
+        mouseDrag: false,   /* Disables mouse click-and-drag scrolling */
+        navText: ["<span class='ion-md-arrow-back'></span>", "<span class='ion-chevron-right'></span>"],
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+    });
+};
 	carousel();
 
 	$('nav .dropdown').hover(function(){
