90d91c325804fc06875177891b9ebc652bfced27
Yasir khan <imyasir.308@gmail.com>
2026-09-17 16:51:25 +0300

    Update portfolio website

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@
 (function($) {
 
 	"use strict";
-/* 
+
 	$(window).stellar({
     responsive: true,
     parallaxBackgrounds: true,
@@ -15,7 +15,6 @@
     hideDistantElements: false,
     scrollProperty: 'scroll'
   });
-  */
 
 
 	var fullHeight = function() {
